c32ae2055b6810bb7a24df9d9f8b2bbaa133ad27
Damian Vidal <dmnvlop@gmail.com>
2025-01-02 14:50:55 +0100

    Fix: APP_KEYS, MODE SHOW

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,13 @@
-export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-  mode: env("MODE", "dev"),
-});
+export default ({ env }) => {
+  console.log("APP_KEYS:", env.array("APP_KEYS"));
+  console.log("MODE:", env("MODE", "dev"));
+
+  return {
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    app: {
+      keys: env.array("APP_KEYS"),
+    },
+    mode: env("MODE", "dev"),
+  };
+};
